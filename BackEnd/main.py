05b26d61362bcbe5c8c87b19cff80e079368fe83
Gabriel Martinello <martinellogabriel@gmail.com>
2023-11-21 21:34:37 -0300

    Adicionando ORM no projeto

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,14 +1,54 @@
 # import main Flask class and request object
-from flask import Flask, request
-from dao.temperaturaDao import TemperaturaDAO
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Resource, Api
+from json import dumps
+from flask_cors import CORS, cross_origin
+from model.temperatura import db, Temperatura, app, temperatura_schema, temperaturas_schema
+from flask_marshmallow import Marshmallow
 
 # create the Flask app
-app = Flask(__name__)
-dao = TemperaturaDAO()
 
-@app.route('/temperatura')
-def query_example():
-    return dao.findAllTemp()
+CORS(app)
+api = Api(app)
+ma = Marshmallow(app)
+
+class Temperaturas(Resource):
+    def get(self):
+        all_temp = Temperatura.query.all()
+        result = temperaturas_schema.dump(all_temp)
+        return make_response(jsonify(result), 200) 
+
+    def post(self):
+        temp = Temperatura(temperatura=request.json['temperatura'], umidade=request.json['umidade'])
+        db.session.add(temp)
+        db.session.commit()
+        result = temperatura_schema.dump(temp)
+        return make_response(jsonify(result.data), 200)
+
+    def put(self):
+        temp = Temperatura.query.filter_by(id=request.json['id']).first()
+        temp.temperatura = request.json['temperatura']
+        temp.umidade = request.json['umidade']
+        db.session.add(temp)
+        db.session.commit()
+        result = temperatura_schema.dump(temp)
+        return make_response(jsonify(result), 200) 
+    
+class TemperaturaById(Resource):
+    def delete(self, id):
+        temp = Temperatura.query.filter_by(id=id).first()
+        db.session.delete(temp)
+        db.session.commit()
+        return make_response(jsonify({"status": "success"}), 200) 
+
+    def get(self, id):
+        temp = Temperatura.query.filter_by(id=id).first()
+        result = temperatura_schema.dump(temp)
+        return make_response(jsonify(result), 200)     
+    
+
+api.add_resource(Temperaturas, '/temperatura') 
+api.add_resource(TemperaturaById, '/temperaturas/<id>') 
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
--- BackEnd/model/temperatura.py
+++ BackEnd/model/temperatura.py
@@ -1,5 +1,34 @@
-class Temperatura:
-    def __init__(self, id, temperatura, umidade):
-        self.id = id
-        self.temperatura = temperatura
-        self.umidade = umidade
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/Temperatura'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Temperatura(db.Model):
+    # __tablename__ = "Temperatura"
+    id = db.Column(db.Integer, primary_key=True)
+    umidade = db.Column(db.String(120), nullable=False)
+    temperatura = db.Column(db.String(120), unique=True, nullable=False)
+
+db.drop_all()
+db.create_all()  
+
+# Dados iniciais
+temperatura = Temperatura(umidade='30', temperatura='25 Graus')
+db.session.add(temperatura)
+db.session.commit()
+
+class TemperaturaSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "umidade", "temperatura")
+
+temperatura_schema = TemperaturaSchema()
+temperaturas_schema = TemperaturaSchema(many=True)        
